9de97a62eacbe94bce2a3878754872088eba8016
João Lima <joaovictorbernardinodelima@gmail.com>
2023-08-05 15:15:42 -0300

    add 2 exercicios

---
+++ lists.py
@@ -0,0 +1,14 @@
+"""Lists
+Create a list that contains at least one string, one integer and one float.
+
+For example:
+
+[1, 'two', 3.14159] 
+
+Note that the order and number of items doesn't matter. 
+
+The answer should just be one list on a single line. Don't assign a variable name to the list.
+"""
+list = ["Joao", 26, 1.79]
+
+print(list)
---
+++ printformat.py
@@ -0,0 +1,18 @@
+"""Print Format.
+Write an expression using anu of the string formatting methods we have learned
+(except f-strings) to return the phrase "Python rules!"
+
+For example, these phrases both return "I like apples":
+    "I like %s"  % "apples"
+    "I like {}".format("apples")
+
+Your solution should be entered on one line. You cant noto use variable names, 
+only the strings themselves.
+
+NOTE: At this time, f-strings won't work!
+Udemy Coding Exercises use Python 3.5.2, and f-strings require Python 3.6 or 
+higher.
+
+"""
+
+print("{}".format("Python rules!"))
